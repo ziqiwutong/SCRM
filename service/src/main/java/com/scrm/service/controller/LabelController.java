409383a05f0743c11685777f1152f2c0702bba09
shuiw <233>
2021-10-14 19:40:46 +0800

    标签CRUD

--- service/src/main/java/com/scrm/service/controller/LabelController.java
+++ service/src/main/java/com/scrm/service/controller/LabelController.java
@@ -1,8 +1,8 @@
 package com.scrm.service.controller;
 
-import com.scrm.service.dao.LabelDao;
 import com.scrm.service.entity.Label;
 import com.scrm.service.service.LabelService;
+import com.scrm.service.util.resp.PageResp;
 import com.scrm.service.util.resp.Resp;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,19 +15,72 @@ public class LabelController {
     @Resource
     private LabelService labelService;
 
-    @Resource
-    private LabelDao labelDao;
+    @GetMapping("/query")
+    @ResponseBody
+    public PageResp query(
+            @RequestParam(value = "pageCount", required = false, defaultValue = "10") Integer pageCount,
+            @RequestParam(value = "currentPage", required = false, defaultValue = "1") Integer currentPage
+    ) {
+        return PageResp.success().setData(
+                labelService.query(pageCount, currentPage)
+        ).setPage(pageCount, currentPage, labelService.queryCount()).setMsg("成功");
+    }
 
     @GetMapping("/queryById")
     @ResponseBody
     public Resp queryById(
             @RequestParam(value = "id") Integer id
     ) {
-        Label label = labelDao.selectById(id);
+        Label label = labelService.queryById(id);
         if (null == label) {
             return Resp.error().setMsg("无数据");
         } else {
             return Resp.success().setData(label).setMsg("成功");
+        }
+    }
+
+    @PostMapping("/insert")
+    @ResponseBody
+    public Resp insert(
+            @RequestBody Label label
+    ) {
+        if (label == null) {
+            return Resp.error().setMsg("不能为空");
+        }
+        String result = labelService.insert(label);
+        if (result == null) {
+            return Resp.success().setData(label).setMsg("插入成功");
+        } else {
+            return Resp.error().setMsg(result);
+        }
+    }
+
+    @PostMapping("/update")
+    @ResponseBody
+    public Resp update(
+            @RequestBody Label label
+    ) {
+        if (label == null) {
+            return Resp.error().setMsg("不能为空");
+        }
+        String result = labelService.update(label);
+        if (result == null) {
+            return Resp.success().setData(label).setMsg("更新成功");
+        } else {
+            return Resp.error().setMsg(result);
+        }
+    }
+
+    @GetMapping("/delete")
+    @ResponseBody
+    public Resp delete(
+            @RequestParam(value = "id") Integer id
+    ) {
+        String result = labelService.delete(id);
+        if (result == null) {
+            return Resp.success().setData(id).setMsg("删除成功");
+        } else {
+            return Resp.error().setMsg(result);
         }
     }
 }
